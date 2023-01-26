dda12e1a6c29db4d4deaad4130b157b6a0a01f66
Stephen-Haven Harris <116969580+StHavenHarris@users.noreply.github.com>
2023-01-26 16:56:28 -0700

    Add files via upload

---
+++ 13_Managing_Inheritance/exercise/src/main/java/com/techelevator/Wall.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+public abstract class Wall {
+    private final String name;
+
+    private final String color;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public Wall(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+
+
+    public abstract int getArea();
+
+
+}
